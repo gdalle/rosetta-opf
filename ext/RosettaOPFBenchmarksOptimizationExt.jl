e7a09d675faa83aae72805e27d8110d46a5765e7
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-06 18:37:15 +0100

    Minimize diff

--- ext/RosettaOPFBenchmarksOptimizationExt.jl
+++ ext/RosettaOPFBenchmarksOptimizationExt.jl
@@ -29,13 +29,13 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
 
     time_model_start = time()
 
-    bus_pd = Dict(i => 0.0 for (i, bus) in ref[:bus])
-    bus_qd = Dict(i => 0.0 for (i, bus) in ref[:bus])
+    bus_pd = Dict(i => 0.0 for (i,bus) in ref[:bus])
+    bus_qd = Dict(i => 0.0 for (i,bus) in ref[:bus])
 
-    bus_gs = Dict(i => 0.0 for (i, bus) in ref[:bus])
-    bus_bs = Dict(i => 0.0 for (i, bus) in ref[:bus])
+    bus_gs = Dict(i => 0.0 for (i,bus) in ref[:bus])
+    bus_bs = Dict(i => 0.0 for (i,bus) in ref[:bus])
 
-    for (i, bus) in ref[:bus]
+    for (i,bus) in ref[:bus]
         if length(ref[:bus_loads][i]) > 0
             bus_pd[i] = sum(ref[:load][l]["pd"] for l in ref[:bus_loads][i])
             bus_qd[i] = sum(ref[:load][l]["qd"] for l in ref[:bus_loads][i])
@@ -48,19 +48,19 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
     end
 
 
-    br_g = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_b = Dict(i => 0.0 for (i, branch) in ref[:branch])
+    br_g = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_b = Dict(i => 0.0 for (i,branch) in ref[:branch])
 
-    br_tr = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_ti = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_ttm = Dict(i => 0.0 for (i, branch) in ref[:branch])
+    br_tr = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_ti = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_ttm = Dict(i => 0.0 for (i,branch) in ref[:branch])
 
-    br_g_fr = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_b_fr = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_g_to = Dict(i => 0.0 for (i, branch) in ref[:branch])
-    br_b_to = Dict(i => 0.0 for (i, branch) in ref[:branch])
+    br_g_fr = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_b_fr = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_g_to = Dict(i => 0.0 for (i,branch) in ref[:branch])
+    br_b_to = Dict(i => 0.0 for (i,branch) in ref[:branch])
 
-    for (i, branch) in ref[:branch]
+    for (i,branch) in ref[:branch]
         g, b = PowerModels.calc_branch_y(branch)
         tr, ti = PowerModels.calc_branch_t(branch)
 
@@ -84,7 +84,7 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
     var_ub = Float64[]
 
     var_idx = 1
-    for (i, bus) in ref[:bus]
+    for (i,bus) in ref[:bus]
         push!(var_init, 0.0) #va
         push!(var_lb, -Inf)
         push!(var_ub, Inf)
@@ -98,7 +98,7 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
         var_idx += 1
     end
 
-    for (i, gen) in ref[:gen]
+    for (i,gen) in ref[:gen]
         push!(var_init, 0.0) #pg
         push!(var_lb, gen["pmin"])
         push!(var_ub, gen["pmax"])
@@ -112,31 +112,31 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
         var_idx += 1
     end
 
-    for (l, i, j) in ref[:arcs]
+    for (l,i,j) in ref[:arcs]
         branch = ref[:branch][l]
 
         push!(var_init, 0.0) #p
         push!(var_lb, -branch["rate_a"])
-        push!(var_ub, branch["rate_a"])
+        push!(var_ub,  branch["rate_a"])
         var_lookup["p_$(l)_$(i)_$(j)"] = var_idx
         var_idx += 1
 
         push!(var_init, 0.0) #q
         push!(var_lb, -branch["rate_a"])
-        push!(var_ub, branch["rate_a"])
+        push!(var_ub,  branch["rate_a"])
         var_lookup["q_$(l)_$(i)_$(j)"] = var_idx
         var_idx += 1
     end
 
-    @assert var_idx == length(var_init) + 1
+    @assert var_idx == length(var_init)+1
 
     #total_callback_time = 0.0
     function opf_objective(x, param)
         #start = time()
         cost = 0.0
-        for (i, gen) in ref[:gen]
+        for (i,gen) in ref[:gen]
             pg = x[var_lookup["pg_$(i)"]]
-            cost += gen["cost"][1] * pg^2 + gen["cost"][2] * pg + gen["cost"][3]
+            cost += gen["cost"][1]*pg^2 + gen["cost"][2]*pg + gen["cost"][3]
         end
         #total_callback_time += time() - start
         return cost
@@ -144,22 +144,22 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
 
     function opf_constraints(ret, x, param)
         #start = time()
-        va = Dict(i => x[var_lookup["va_$(i)"]] for (i, bus) in ref[:bus])
-        vm = Dict(i => x[var_lookup["vm_$(i)"]] for (i, bus) in ref[:bus])
+        va = Dict(i => x[var_lookup["va_$(i)"]] for (i,bus) in ref[:bus])
+        vm = Dict(i => x[var_lookup["vm_$(i)"]] for (i,bus) in ref[:bus])
 
-        pg = Dict(i => x[var_lookup["pg_$(i)"]] for (i, gen) in ref[:gen])
-        qg = Dict(i => x[var_lookup["qg_$(i)"]] for (i, gen) in ref[:gen])
+        pg = Dict(i => x[var_lookup["pg_$(i)"]] for (i,gen) in ref[:gen])
+        qg = Dict(i => x[var_lookup["qg_$(i)"]] for (i,gen) in ref[:gen])
 
-        p = Dict((l, i, j) => x[var_lookup["p_$(l)_$(i)_$(j)"]] for (l, i, j) in ref[:arcs])
-        q = Dict((l, i, j) => x[var_lookup["q_$(l)_$(i)_$(j)"]] for (l, i, j) in ref[:arcs])
+        p = Dict((l,i,j) => x[var_lookup["p_$(l)_$(i)_$(j)"]] for (l,i,j) in ref[:arcs])
+        q = Dict((l,i,j) => x[var_lookup["q_$(l)_$(i)_$(j)"]] for (l,i,j) in ref[:arcs])
 
-        vm_fr = Dict(l => vm[branch["f_bus"]] for (l, branch) in ref[:branch])
-        vm_to = Dict(l => vm[branch["t_bus"]] for (l, branch) in ref[:branch])
-        va_fr = Dict(l => va[branch["f_bus"]] for (l, branch) in ref[:branch])
-        va_to = Dict(l => va[branch["t_bus"]] for (l, branch) in ref[:branch])
+        vm_fr = Dict(l => vm[branch["f_bus"]] for (l,branch) in ref[:branch])
+        vm_to = Dict(l => vm[branch["t_bus"]] for (l,branch) in ref[:branch])
+        va_fr = Dict(l => va[branch["f_bus"]] for (l,branch) in ref[:branch])
+        va_to = Dict(l => va[branch["t_bus"]] for (l,branch) in ref[:branch])
 
 
-        va_con = [va[i] for (i, bus) in ref[:ref_buses]]
+        va_con = [va[i] for (i,bus) in ref[:ref_buses]]
 
         #     @constraint(model,
         #         sum(p[a] for a in ref[:bus_arcs][i]) ==
@@ -168,11 +168,11 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
         #         sum(shunt["gs"] for shunt in bus_shunts)*vm[i]^2
         #     )
         power_balance_p_con = [
-            sum(pg[j] for j in ref[:bus_gens][i]; init=0.0) -
-            bus_pd[i] -
-            bus_gs[i] * vm[i]^2 -
-            sum(p[a] for a in ref[:bus_arcs][i])
-            for (i, bus) in ref[:bus]
+           sum(pg[j] for j in ref[:bus_gens][i]; init=0.0) -
+           bus_pd[i] -
+           bus_gs[i]*vm[i]^2 -
+           sum(p[a] for a in ref[:bus_arcs][i])
+           for (i,bus) in ref[:bus]
         ]
 
         #     @constraint(model,
@@ -182,67 +182,67 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
         #         sum(shunt["bs"] for shunt in bus_shunts)*vm[i]^2
         #     )
         power_balance_q_con = [
-            sum(qg[j] for j in ref[:bus_gens][i]; init=0.0) -
-            bus_qd[i] +
-            bus_bs[i] * vm[i]^2 -
-            sum(q[a] for a in ref[:bus_arcs][i])
-            for (i, bus) in ref[:bus]
+           sum(qg[j] for j in ref[:bus_gens][i]; init=0.0) -
+           bus_qd[i] +
+           bus_bs[i]*vm[i]^2 -
+           sum(q[a] for a in ref[:bus_arcs][i])
+           for (i,bus) in ref[:bus]
         ]
 
 
         # @NLconstraint(model, p_fr ==  (g+g_fr)/ttm*vm_fr^2 + (-g*tr+b*ti)/ttm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/ttm*(vm_fr*vm_to*sin(va_fr-va_to)) )
         power_flow_p_from_con = [
-            (br_g[l] + br_g_fr[l]) / br_ttm[l] * vm_fr[l]^2 +
-            (-br_g[l] * br_tr[l] + br_b[l] * br_ti[l]) / br_ttm[l] * (vm_fr[l] * vm_to[l] * cos(va_fr[l] - va_to[l])) +
-            (-br_b[l] * br_tr[l] - br_g[l] * br_ti[l]) / br_ttm[l] * (vm_fr[l] * vm_to[l] * sin(va_fr[l] - va_to[l])) -
-            p[(l, i, j)]
-            for (l, i, j) in ref[:arcs_from]
+           (br_g[l]+br_g_fr[l])/br_ttm[l]*vm_fr[l]^2 +
+           (-br_g[l]*br_tr[l]+br_b[l]*br_ti[l])/br_ttm[l]*(vm_fr[l]*vm_to[l]*cos(va_fr[l]-va_to[l])) +
+           (-br_b[l]*br_tr[l]-br_g[l]*br_ti[l])/br_ttm[l]*(vm_fr[l]*vm_to[l]*sin(va_fr[l]-va_to[l])) -
+           p[(l,i,j)]
+           for (l,i,j) in ref[:arcs_from]
         ]
 
         # @NLconstraint(model, p_to ==  (g+g_to)*vm_to^2 + (-g*tr-b*ti)/ttm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-b*tr+g*ti)/ttm*(vm_to*vm_fr*sin(va_to-va_fr)) )
         power_flow_p_to_con = [
-            (br_g[l] + br_g_to[l]) * vm_to[l]^2 +
-            (-br_g[l] * br_tr[l] - br_b[l] * br_ti[l]) / br_ttm[l] * (vm_to[l] * vm_fr[l] * cos(va_to[l] - va_fr[l])) +
-            (-br_b[l] * br_tr[l] + br_g[l] * br_ti[l]) / br_ttm[l] * (vm_to[l] * vm_fr[l] * sin(va_to[l] - va_fr[l])) -
-            p[(l, i, j)]
-            for (l, i, j) in ref[:arcs_to]
+           (br_g[l]+br_g_to[l])*vm_to[l]^2 +
+           (-br_g[l]*br_tr[l]-br_b[l]*br_ti[l])/br_ttm[l]*(vm_to[l]*vm_fr[l]*cos(va_to[l]-va_fr[l])) +
+           (-br_b[l]*br_tr[l]+br_g[l]*br_ti[l])/br_ttm[l]*(vm_to[l]*vm_fr[l]*sin(va_to[l]-va_fr[l])) -
+           p[(l,i,j)]
+           for (l,i,j) in ref[:arcs_to]
         ]
 
         # @NLconstraint(model, q_fr == -(b+b_fr)/ttm*vm_fr^2 - (-b*tr-g*ti)/ttm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/ttm*(vm_fr*vm_to*sin(va_fr-va_to)) )
         power_flow_q_from_con = [
-            -(br_b[l] + br_b_fr[l]) / br_ttm[l] * vm_fr[l]^2 -
-            (-br_b[l] * br_tr[l] - br_g[l] * br_ti[l]) / br_ttm[l] * (vm_fr[l] * vm_to[l] * cos(va_fr[l] - va_to[l])) +
-            (-br_g[l] * br_tr[l] + br_b[l] * br_ti[l]) / br_ttm[l] * (vm_fr[l] * vm_to[l] * sin(va_fr[l] - va_to[l])) -
-            q[(l, i, j)]
-            for (l, i, j) in ref[:arcs_from]
+           -(br_b[l]+br_b_fr[l])/br_ttm[l]*vm_fr[l]^2 -
+           (-br_b[l]*br_tr[l]-br_g[l]*br_ti[l])/br_ttm[l]*(vm_fr[l]*vm_to[l]*cos(va_fr[l]-va_to[l])) +
+           (-br_g[l]*br_tr[l]+br_b[l]*br_ti[l])/br_ttm[l]*(vm_fr[l]*vm_to[l]*sin(va_fr[l]-va_to[l])) -
+           q[(l,i,j)]
+           for (l,i,j) in ref[:arcs_from]
         ]
 
         # @NLconstraint(model, q_to == -(b+b_to)*vm_to^2 - (-b*tr+g*ti)/ttm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-g*tr-b*ti)/ttm*(vm_to*vm_fr*sin(va_to-va_fr)) )
         power_flow_q_to_con = [
-            -(br_b[l] + br_b_to[l]) * vm_to[l]^2 -
-            (-br_b[l] * br_tr[l] + br_g[l] * br_ti[l]) / br_ttm[l] * (vm_to[l] * vm_fr[l] * cos(va_to[l] - va_fr[l])) +
-            (-br_g[l] * br_tr[l] - br_b[l] * br_ti[l]) / br_ttm[l] * (vm_to[l] * vm_fr[l] * sin(va_to[l] - va_fr[l])) -
-            q[(l, i, j)]
-            for (l, i, j) in ref[:arcs_to]
+           -(br_b[l]+br_b_to[l])*vm_to[l]^2 -
+           (-br_b[l]*br_tr[l]+br_g[l]*br_ti[l])/br_ttm[l]*(vm_to[l]*vm_fr[l]*cos(va_to[l]-va_fr[l])) +
+           (-br_g[l]*br_tr[l]-br_b[l]*br_ti[l])/br_ttm[l]*(vm_to[l]*vm_fr[l]*sin(va_to[l]-va_fr[l])) -
+           q[(l,i,j)]
+           for (l,i,j) in ref[:arcs_to]
         ]
 
         # @constraint(model, va_fr - va_to <= branch["angmax"])
         # @constraint(model, va_fr - va_to >= branch["angmin"])
         power_flow_vad_con = [
-            va_fr[l] - va_to[l]
-            for (l, i, j) in ref[:arcs_from]
+           va_fr[l] - va_to[l]
+           for (l,i,j) in ref[:arcs_from]
         ]
 
         # @constraint(model, p_fr^2 + q_fr^2 <= branch["rate_a"]^2)
         power_flow_mva_from_con = [
-            p[(l, i, j)]^2 + q[(l, i, j)]^2
-            for (l, i, j) in ref[:arcs_from]
+           p[(l,i,j)]^2 + q[(l,i,j)]^2
+           for (l,i,j) in ref[:arcs_from]
         ]
 
         # @constraint(model, p_to^2 + q_to^2 <= branch["rate_a"]^2)
         power_flow_mva_to_con = [
-            p[(l, i, j)]^2 + q[(l, i, j)]^2
-            for (l, i, j) in ref[:arcs_to]
+           p[(l,i,j)]^2 + q[(l,i,j)]^2
+           for (l,i,j) in ref[:arcs_to]
         ]
         ret .= [
             va_con...,
@@ -263,63 +263,63 @@ function Rosetta.solve_opf(file_name, ::Val{:Optimization})
     con_ubs = Float64[]
 
     #@constraint(model, va[i] == 0)
-    for (i, bus) in ref[:ref_buses]
+    for (i,bus) in ref[:ref_buses]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_balance_p_con
-    for (i, bus) in ref[:bus]
+    for (i,bus) in ref[:bus]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_balance_q_con
-    for (i, bus) in ref[:bus]
+    for (i,bus) in ref[:bus]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_flow_p_from_con
-    for (l, i, j) in ref[:arcs_from]
+    for (l,i,j) in ref[:arcs_from]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_flow_p_to_con
-    for (l, i, j) in ref[:arcs_to]
+    for (l,i,j) in ref[:arcs_to]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_flow_q_from_con
-    for (l, i, j) in ref[:arcs_from]
+    for (l,i,j) in ref[:arcs_from]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_flow_q_to_con
-    for (l, i, j) in ref[:arcs_to]
+    for (l,i,j) in ref[:arcs_to]
         push!(con_lbs, 0.0)
         push!(con_ubs, 0.0)
     end
 
     #power_flow_vad_con
-    for (l, i, j) in ref[:arcs_from]
+    for (l,i,j) in ref[:arcs_from]
         branch = ref[:branch][l]
         push!(con_lbs, branch["angmin"])
         push!(con_ubs, branch["angmax"])
     end
 
     #power_flow_mva_from_con
-    for (l, i, j) in ref[:arcs_from]
+    for (l,i,j) in ref[:arcs_from]
         branch = ref[:branch][l]
         push!(con_lbs, -Inf)
         push!(con_ubs, branch["rate_a"]^2)
     end
 
     #power_flow_mva_to_con
-    for (l, i, j) in ref[:arcs_to]
+    for (l,i,j) in ref[:arcs_to]
         branch = ref[:branch][l]
         push!(con_lbs, -Inf)
         push!(con_ubs, branch["rate_a"]^2)
